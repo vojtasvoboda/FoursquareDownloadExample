fcb48685ba3f36cd00923b35823696e2df364ffa
Vojta Svoboda <vojtasvoboda.cz@gmail.com>
2014-06-28 18:18:10 +0200

    File format

--- index.php
+++ index.php
@@ -11,7 +11,8 @@ require 'libs/functions.inc.php';
 Tracy\Debugger::enable();
 
 // set library for downloading data
-$client = new \TheTwelve\Foursquare\HttpClient\CurlHttpClient(\Kdyby\CurlCaBundle\CertificateHelper::getCaInfoFile());
+$caCertificate = \Kdyby\CurlCaBundle\CertificateHelper::getCaInfoFile();
+$client = new \TheTwelve\Foursquare\HttpClient\CurlHttpClient($caCertificate);
 $factory = new \TheTwelve\Foursquare\ApiGatewayFactory($client);
 $factory->setClientCredentials(
     'CLIENT_ID',
@@ -42,7 +43,13 @@ $options = array(
 $connection = new DibiConnection($options);
 
 // allow database fields
-$allowFields = array('place_id', 'name', 'contact_phone', 'contact_formattedPhone', 'contact_twitter', 'location_address', 'location_crossStreet', 'location_lat', 'location_lng', 'location_distance', 'location_postalCode', 'location_cc', 'location_city', 'location_state', 'location_country', 'location_formattedAddress_0', 'location_formattedAddress_1', 'categories_0_id', 'categories_0_name', 'verified', 'stats_checkinsCount', 'stats_usersCount', 'stats_tipCount', 'url', 'specials_count', 'specials_items', 'hereNow_count', 'hereNow_summary', 'referralId');
+$allowFields = array(
+    'place_id', 'name', 'contact_phone', 'contact_formattedPhone', 'contact_twitter', 'location_address',
+    'location_crossStreet', 'location_lat', 'location_lng', 'location_distance', 'location_postalCode', 'location_cc',
+    'location_city', 'location_state', 'location_country', 'location_formattedAddress_0', 'location_formattedAddress_1',
+    'categories_0_id', 'categories_0_name', 'verified', 'stats_checkinsCount', 'stats_usersCount', 'stats_tipCount',
+    'url', 'specials_count', 'specials_items', 'hereNow_count', 'hereNow_summary', 'referralId'
+);
 
 // save venues
 foreach ($venues as $venue) {
